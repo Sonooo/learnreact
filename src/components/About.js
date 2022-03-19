434bc2fa1c265c39201f17b40512f78e6c264cc8
saurabh <35056406+Sonooo@users.noreply.github.com>
2022-03-19 12:55:18 +0530

    changes made

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
-export default function About() {
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // }) 
+    let myStyle = {
+        color: props.mode === 'dark' ? 'white' : '#042743',
+        backgroundColor: props.mode === 'dark' ? 'rgb(36 74 104)' : 'white',
+    }
+
     return (
-        <div className='cotainer text-light'>
+        <div className="container" style={myStyle}>
             <h1 className='text-center'>about us</h1>
             <p className='text-center'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo nemo quisquam dolore natus dicta error quod, culpa amet harum animi, eos molestias vero consectetur tempora praesentium tempore aspernatur recusandae cupiditate.
@@ -16,6 +26,7 @@ export default function About() {
                 Iure cumque nobis dolores autem! Quos nulla inventore explicabo corporis exercitationem? Omnis cupiditate saepe, porro placeat similique suscipit aspernatur id iste adipisci sunt quibusdam ad ipsam dolore vitae modi consequatur.
                 Ut veritatis laborum eos ducimus aliquid maxime ad, itaque iure quidem in nihil amet eaque perferendis ratione saepe molestias dignissimos adipisci repellat iste exercitationem quaerat quasi culpa tempore est! Ipsa.
             </p>
+
         </div>
     )
 }
